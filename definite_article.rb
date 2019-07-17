c545aa7b77b998289b9974e71e352e23893df3e2
Kangjin Jun <kangjin.jun@tomtom.com>
2019-07-17 16:47:31 +0200

    help func added

--- definite_article.rb
+++ definite_article.rb
@@ -1,10 +1,8 @@
 #!/usr/bin/env ruby
-
-
-genders = ['그 남자', '그 개', '그 여자', '그 사람들']
+genders = ['그 남자', '그 아이', '그 여자', '그 사람들']
 components = ['가(이)', '를(을)', '에게', '의']
 
-subjects 	= ['Mann', 'Hund', 'Frau', 'Leute']
+subjects 	= ['Mann', 'Kind', 'Frau', 'Leute']
 answers 	= [
 				['der', 'den', 'dem', 'des'],
 				['das', 'das', 'dem', 'des'],
@@ -14,6 +12,13 @@ answers 	= [
 
 definte_idx = (rand * 100 % 4).to_i 
 subject_idx = (rand * 100 % 4).to_i 
+
+if ARGV[0] == 'help' and !ARGV[1].nil?
+  ['Nom(가)', 'Akk(를)', 'Dat(에게)', 'Gen(의)'].zip(answers[ARGV[1].to_i - 1]).each{|x|
+	  puts "#{x[0]} : #{x[1]}"
+  }
+  exit
+end
 
 if ARGV.nil? == false and ARGV[0].nil? == false
   subject_idx = ARGV[0].to_i - 1
