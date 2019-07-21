ad7838dc0fe9f740fd96aa96f2fb500e72152cb9
Kangjin Jun <kangjinjun@Kangjins-Mac-mini-3.local>
2019-07-21 16:41:16 +0200

    refactoring #2

--- german_data.rb
+++ german_data.rb
@@ -5,19 +5,38 @@ class GermanData
 		@doc = YAML.load_file './definite_article.yaml'
 	end
 	public
+	def get_random_noun(gender)
+		noun_pair = @doc['nouns'][gender].sample
+		return noun_pair.keys[0], noun_pair[noun_pair.keys[0]]
+	end
 	def genders
-		@doc['genders_for_definite']
+		%w(masculine neutral feminine plural)
+	end
+	def genders_for_indefinite
+		%w(masculine neutral feminine)
+	end
+	def genders_with_all
+		genders.insert(0, 'all')
+	end
+	def genders_for_indefinite_with_all
+		genders = genders_for_indefinite.insert(0, 'all')
+		genders.delete('plural')
+		genders
 	end
 	def cases
 		@doc['cases']
 	end
-	def nouns
-		@doc['nouns']
+	def get_random_case
+		case_pair = cases.sample
+		return case_pair.keys[0], case_pair[case_pair.keys[0]]
 	end
 	def article_types
-		@doc['article_types']
+		%w(definite_articles indefinite_articles)
 	end
 	def definite_articles
 		@doc['definite_articles']
+	end
+	def indefinite_articles
+		@doc['indefinite_articles']
 	end
 end
--- german_grammarbook.rb
+++ german_grammarbook.rb
@@ -12,23 +12,35 @@ class GermanGrammarCLI < Thor
 		@prompt = TTY::Prompt.new
 	end
 
+	
+
 	private
-	def get_definite_article_qna(article, gender)
+	def get_ko_article(article)
+		case article
+		when 'indefinite_article'
+			return '한'
+		when 'definite_article'
+			return '그'
+		end
+	end
+	def get_article_qna(article, gender)
 		if gender == 'all' 
-			gender = @german_data.genders.sample
+			genders = nil
+			case article
+			when 'indefinite_articles'
+				genders = @german_data.genders_for_indefinite
+			when 'definite_articles'
+				genders = @german_data.genders
+			end
+			gender = genders.sample
 		end
 
-	    noun_pair = @german_data.nouns[gender].sample
-	    noun_ger = noun_pair.keys[0]
-	    noun_kor = noun_pair[noun_ger]
-
-	    case_pair = @german_data.cases.sample
-	    case_ger = case_pair.keys[0]
-	    case_kor = case_pair[case_ger]
+		noun_ger, noun_kor = @german_data.get_random_noun(gender)
+		case_ger, case_kor = @german_data.get_random_case
 
 		{
-			:question	=> "그 #{noun_kor}#{case_kor}?",
-			:answer		=> "#{@german_data.definite_articles[gender][case_ger].red} #{noun_ger}"
+			:question	=> "#{get_ko_article(article)} #{noun_kor}#{case_kor}?",
+			:answer		=> "#{@german_data.send(article)[gender][case_ger].red} #{noun_ger}"
 		}
 	end
 
@@ -49,14 +61,15 @@ class GermanGrammarCLI < Thor
 			article = @prompt.enum_select("which type do you want to study?", @german_data.article_types)
 		end
 		if(gender.nil?)
-			gender = @prompt.enum_select("Select a gender?", @german_data.genders.clone.insert(0, 'all'))
+			genders = article == 'definite_articles' ? @german_data.genders_with_all : @german_data.genders_for_indefinite_with_all
+			gender = @prompt.enum_select("Select a gender?", genders) 
 		end
 		puts '---------------------------------------'
 
 		cnt = 0
 		while(1) do 
 			cnt += 1
-			qna = get_definite_article_qna(article, gender)
+			qna = get_article_qna(article, gender)
 			question = "Q#{cnt}. #{qna[:question]}"
 			answer = " => A#{cnt}. #{qna[:answer]}"
 
